d6bc2ab1cb7c7e75f3df3330eb306334630b7c61
4door <shiorishinohe@gmail.com>
2020-12-18 23:02:22 +0900

    update:header

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,14 @@
-
 <template>
   <v-app>
-    <v-content>
-      <v-container fluid>
-        <v-layout row wrap>
-          <v-flex v-for="item in items" v-bind:key="item.id" xs12 sm6 md4>
-            <Card v-bind:title="item.title" v-bind:content="item.content"/>
-          </v-flex>
-        </v-layout>
-      </v-container>
-    </v-content>
-  <!-- 後略 -->
+    <AppHeader/>
   </v-app>
 </template>
-
 <script>
-import Card from './components/Card.vue'
-
+import AppHeader from './components/AppHeader.vue'
 export default {
   name: 'App',
   components: {
-    Card
-  },
-  data () {
-    return {
-      items: [
-      { title: 'このサイトについて', content: 'ソフトウェアエンジニア"4door"のポートフォリオサイトです。\n技術的には、Vue.js＋Vuetifyで作成し、GitHubで管理して、Netlifyにホスティングして公開しています。' },
-      { title: 'カード02', content: 'カード02の内容です' },
-      { title: 'カード03', content: 'カード03の内容です' },
-      ]
-    }
+    AppHeader
   }
 }
 </script>
