51ece2ef8d60a38d1725619fe80a0c36ff9d2bd0
4door <shiorishinohe@gmail.com>
2020-12-19 02:40:42 +0900

    update:about&work

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,5 +1,20 @@
 <template>
   <section class="about">
-    <h2>About</h2>
+    <v-container fluid fill-height class="about__content">
+        <h2>About</h2>
+    </v-container>
   </section>
 </template>
+<style lang="scss" scope>
+@import '../styles/common/common.scss';
+.about__content {
+  background: url("../assets/top.jpg");
+  background-size: cover;
+  background-position: center center;
+  width: 100%;
+  height: 50vh;
+  @include display_pc {
+    height: 100vh;
+  }
+}
+</style>
--- src/components/Work.vue
+++ src/components/Work.vue
@@ -1,5 +1,20 @@
 <template>
   <section class="work">
-    <h2>Work</h2>
+    <v-container fluid fill-height class="work__content">
+      <h2 h2>Work</h2>
+    </v-container>
   </section>
 </template>
+<style lang="scss" scope>
+@import '../styles/common/common.scss';
+.work__content {
+  background: url("../assets/top.jpg");
+  background-size: cover;
+  background-position: center center;
+  width: 100%;
+  height: 50vh;
+  @include display_pc {
+    height: 100vh;
+  }
+}
+</style>
