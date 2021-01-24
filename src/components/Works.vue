41260ac35222a389d56728313a2fea2c8d64f4e2
4door <shiorishinohe@gmail.com>
2021-01-24 23:06:20 +0900

    works

--- src/components/Works.vue
+++ src/components/Works.vue
@@ -1,22 +1,48 @@
 <template>
-  <section class="skills">
-    <v-container fluid fill-height class="skills__content">
+  <section class="works">
+    <v-container fluid fill-height class="works__content">
       <b-row style="margin: 0 0 auto;">
         <b-card
-          title="制作物"
+          title="Task List"
           class="mb-2"
         >
           <b-card-text>
-            ただいま準備中
+            タスク管理アプリです。
+            Rubyの学習に作成。
           </b-card-text>
         </b-card>
-      </b-row>
+        <b-carousel
+          id="carousel-no-animation"
+          style="text-shadow: 0px 0px 2px #000"
+          no-animation
+          indicators
+          img-width="1024"
+          img-height="480"
+        >
+          <b-carousel-slide
+            caption="First slide"
+            img-src="https://picsum.photos/1024/480/?image=10"
+          ></b-carousel-slide>
+          <b-carousel-slide
+            caption="Second Slide"
+            img-src="https://picsum.photos/1024/480/?image=12"
+          ></b-carousel-slide>
+          <b-carousel-slide
+            caption="Third Slide"
+            img-src="https://picsum.photos/1024/480/?image=22"
+          ></b-carousel-slide>
+          <b-carousel-slide
+            caption="Fourth Slide"
+            img-src="https://picsum.photos/1024/480/?image=23"
+          ></b-carousel-slide>
+        </b-carousel>
+      </b-row>      
     </v-container>
   </section>
 </template>
 <style lang="scss" scope>
 @import '../styles/common/common.scss';
-.skills__content {
+.works__content {
   background: url("../assets/top.jpg");
   background-size: cover;
   background-position: center center;
@@ -26,5 +52,12 @@
 .mb-2 {  
     margin-right: 20px;
     width: 25rem;
+}
+
+element.style {
+    height: 500px;
+}
+.v-carousel {
+  width: 50% !important;
 }
 </style>
